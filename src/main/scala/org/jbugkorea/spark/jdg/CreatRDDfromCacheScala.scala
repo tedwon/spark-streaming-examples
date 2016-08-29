33cef7c5b0af01f852f40131f36e8f1676014fc2
Ted Won <iamtedwon@gmail.com>
2016-08-29 17:56:33 +0900

    Update  Creating a RDD from cache

--- src/main/scala/org/jbugkorea/spark/jdg/CreatRDDfromCacheScala.scala
+++ src/main/scala/org/jbugkorea/spark/jdg/CreatRDDfromCacheScala.scala
@@ -17,22 +17,25 @@ object CreatRDDfromCacheScala {
     val cacheManager = new RemoteCacheManager
     val cache: RemoteCache[Integer, User] = cacheManager.getCache()
 
-    // put some sample data
-    cache.put(1, new User("a", 1))
-    cache.put(2, new User("b", 2))
+    // Put some sample data to the remote cache
+    cache.put(1, new User("ted", 1))
+    cache.put(2, new User("bob", 2))
 
 
     val infinispanHost = "127.0.0.1:11222;127.0.0.1:11372"
 
-    val conf = new SparkConf().setAppName("spark-infinispan-wordcount-scala")
+    val conf = new SparkConf()
+      .setAppName("spark-infinispan-wordcount-scala")
       .setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     val infinispanProperties = new Properties
     infinispanProperties.put("infinispan.client.hotrod.server_list", infinispanHost)
-    //    infinispanProperties.put("infinispan.rdd.cacheName", "exampleCache")
+    infinispanProperties.put("infinispan.rdd.cacheName", "default")
+
+    // Create RDD from cache
     val infinispanRDD = new InfinispanRDD[Integer, User](sc, configuration = infinispanProperties)
-    // create RDD from cache
+    infinispanRDD.foreach(println)
     val usersRDD = infinispanRDD.values.count()
 
     println(usersRDD)
